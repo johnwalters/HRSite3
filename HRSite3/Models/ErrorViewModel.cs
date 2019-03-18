25065544c0390d36778bbdff36a01816cee7a94b
johnwalters <john.walters.92117@gmail.com>
2019-03-17 18:06:21 -0700

    Add project files.

---
+++ HRSite3/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HRSite3.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
